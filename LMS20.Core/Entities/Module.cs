2ce6769d0bf3de2d9dd87be91a2ba8fb53b2e845
johanneswf <johanneswf@gmail.com>
2022-09-08 17:03:40 +0200

    Add project files.

---
+++ LMS20.Core/Entities/Module.cs
@@ -0,0 +1,17 @@
+﻿namespace LMS20.Core.Entities
+{
+    public class Module
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime StartDateTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public ICollection<ModuleActivity> ModuleActivities { get; set; } = new List<ModuleActivity>();
+        public ICollection<Document> Documents { get; set; } = new List<Document>();
+        public Course Course { get; set; } = new();
+
+        public int CourseId { get; set; }
+    }
+}
---
+++ LMS20.Core/Entities/ModuleActivity.cs
@@ -0,0 +1,15 @@
+﻿namespace LMS20.Core.Entities
+{
+    public class ModuleActivity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime StartDateTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        public ICollection<Document> Documents { get; set; } = new List<Document>();
+
+        public int ModuleId { get; set; }
+    }
+}
