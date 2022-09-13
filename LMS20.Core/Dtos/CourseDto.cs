9b2c40fe97b1bb8c3bd9796b0b657cf4071c815f
Klara Höglund <klarahoglund@hotmail.com>
2022-09-13 12:58:52 +0200

    Dtos mm

---
+++ LMS20.Core/Dtos/CourseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace LMS20.Core.Dtos
+{
+    public class CourseDto
+    {
+        
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime StartDateTime { get; set; }
+    
+        public TimeSpan Duration { get; set; }
+
+      
+    }
+}
--- LMS20.Core/Entities/ModuleActivity.cs
+++ LMS20.Core/Entities/ModuleActivity.cs
@@ -7,7 +7,7 @@
         public string Description { get; set; } = string.Empty;
         public DateTime StartDateTime { get; set; }
         public TimeSpan Duration { get; set; }
-
+         
         public ICollection<Document> Documents { get; set; } = new List<Document>();
 
         public Module Module { get; set; }
