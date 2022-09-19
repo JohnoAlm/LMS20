504b7028f430d74bea10081040fa694811fd533e
almenoliver02@gmail.com <almenoliver02@gmail.com>
2022-09-19 13:01:47 +0200

    Changed seed to have a start and end date. Haven't tested

--- LMS20.Data/Data/SeedData.cs
+++ LMS20.Data/Data/SeedData.cs
@@ -152,8 +152,8 @@ namespace LMS20.Data.Data
                 {
                     Name = $"Programmering {i}",
                     Description = faker.Company.Bs(),
-                    StartDateTime = DateTime.Now.AddMinutes(5),
-                    Duration = new TimeSpan(0, 15, 0),
+                    Start = DateTime.Now.AddMinutes(5),
+                    End = new DateTime(2023, 09, 19),
                     Modules = GetModules()
                 };
 
@@ -182,8 +182,8 @@ namespace LMS20.Data.Data
                 {
                     Name = "Modul 1",
                     Description = faker.Company.Bs(),
-                    StartDateTime = DateTime.Now,
-                    Duration = new TimeSpan(0, 55, 0),
+                    Start = DateTime.Now,
+                    End = new DateTime(2022, 10, 19),
                     //CourseId = course.Id
                     ModuleActivities = GetModuleActivities()
                     
@@ -210,8 +210,8 @@ namespace LMS20.Data.Data
                 {
                     Name = "Föreläsning",
                     Description = faker.Company.Bs(),
-                    StartDateTime = DateTime.Now,
-                    Duration = new TimeSpan(0, 55, 0),
+                    Start = DateTime.Now,
+                    End = new DateTime(2022, 09, 26),
                     //ModuleId = module.Id
                     
                 };
