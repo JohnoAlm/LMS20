7ae67dc7f39a1aec2b9e14dd6cd53938e66b5bfc
klarahoglund <klarahoglund@hotmail.com>
2022-09-27 13:12:10 +0200

    ändrat namn i Particiant vm

--- LMS20.Core/ViewModels/ParticipantsViewModel.cs
+++ LMS20.Core/ViewModels/ParticipantsViewModel.cs
@@ -5,8 +5,8 @@ namespace LMS20.Core.ViewModels
 #nullable disable
     public class ParticipantsViewModel
     {
-        public int CourseId { get; set; }
-        public string CourseName { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
         public IEnumerable<ApplicationUser> ApplicationUsers { get; set; } = new List<ApplicationUser>();
         public RegistrationViewModel RegistrationViewModel { get; set; }
         public EditUserViewModel EditUserViewModel { get; set; }
